28fba32a67cc48506e6661fd9feffe2e380c8d6e
Anna Korotkova <anna.korotkova@mail.schwarz>
2020-06-22 19:59:55 +0300

    Fixed some bugs, improved template.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import * as jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  @ViewChild('htmlData') htmlData: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  public openPDF(): void {
+    const data = document.getElementById('htmlData');
+    html2canvas(data).then(canvas => {
+      let width = canvas.width;
+      let height = canvas.height;
+
+      const  imgData = canvas.toDataURL('image/jpeg');
+      const  doc = new jsPDF('p', 'mm', 'a4', true);
+      width = doc.internal.pageSize.width;
+      height = doc.internal.pageSize.height;
+      const img = new Image(),
+        ctx = canvas.getContext('2d');
+
+      console.log(doc);
+
+      const divHeight = this.htmlData.nativeElement.offsetHeight;
+      const divWidth = this.htmlData.nativeElement.offsetWidth;
+      const ratio = divHeight / divWidth;
+      // doc.addPage();
+       //doc.addImage(imgData, 'JPEG', 0, 0);//, 485, 270, undefined,'FAST');
+      doc.addImage(imgData, 'JPEG', 0, -30, width, ratio*height, undefined, 'FAST');
+      // //doc.addImage(imgData, 'JPEG', 0, 0);
+      doc.save('WebSiteScreen.pdf');
+
+    });
   }
 
 }
--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -23,47 +23,68 @@ export class PreviewComponent implements OnInit {
 
   constructor(private uploadService: UploadService ) { }
 
+  strip(html) {
+    const tmp = document.implementation.createHTMLDocument('New').body;
+    tmp.innerHTML = html;
+    return tmp.textContent || tmp.innerText || '';
+  }
+
   ngOnInit(): void {
-    this.res = JSON.parse(localStorage.getItem('resume'));
-    const e: Educ = { start: new Date(), end: new Date(), degree: 'MS', school: 'Smolensk state university',
-                      decrpt: 'I am an energetic, ambitious person who has developed a mature and responsible approach to any task that I undertake, or situation that I am presented with.', city: 'Smolensl', level: ' ', years: ' '};
-    this.eduArray.push(e);
-    const empl: Empl = { start: new Date(), end: new Date(), job: 'Junior Full-Stack devdloper', empl: 'IT HUB Kaufland',
-      decrpt: 'I am a mature, positive and hardworking individual, who always strives to achieve the highest standard ' +
-        'possible, at any given task. In my previous role as a Sales Representative, I demonstrated the ability to work under ' +
-        'intense pressure, sell products and services to customers from all backgrounds, handle customer complaints and solve ' +
-        'problematic situations as and when they arose. I was promoted twice for exceeding my sales targets.', city: 'Smolensk'};
-    this.emplArray.push(empl);
-    const l1: Lang = {language: 'German', level: 'Beginner'};
-    const l2: Lang = {language: 'English', level: 'Beginner'};
-    this.langArray.push(l1);
-    this.langArray.push(l2);
+    this.resume = JSON.parse(localStorage.getItem('resume'));
+    this.resume.edu.map(e => e.decrpt = this.strip(e.decrpt));
+    this.resume.empl.map(e => e.decrpt = this.strip(e.decrpt));
 
-    const skill1: Skill = {skillValue: 8, skill: 'C++'};
-    const skill2: Skill = {skillValue: 6, skill: 'Java'};
-    this.skillsArray.push(skill1);
-    this.skillsArray.push(skill2);
-    this.skillsArray.push(skill2);
-    this.skillsArray.push(skill2);
-    this.skillsArray.push(skill2);
-    this.skillsArray.push(skill2);
-    this.skillsArray.push(skill2);
-    this.skillsArray.push(skill2);
-    this.skillsArray.push(skill2);
+    document.getElementById('quill').innerHTML =  this.resume.profile;
+    //document.getElementById('cource').innerHTML = this.resume.cources;
 
-    this.resume = {imagePreview: null, imagePath: 'this.res.imagePath',
-      firstName: 'Anna', secondName: 'Korotkova',
-      country: 'Bulgaria', city: 'Sofia Mladost 1a', citizenShip: 'Russia',
-      postCode: 'AnnaAhmatoba bl.532 1720', phone: '+359123456', email: 'anna@bg.bg', web: 'www.web',
-      skype: 'my_skype', gitHub: '/aneova', profile: 'I have a clear, logical mind with a practical approach to ' +
-        'problem-solving and a drive to see things through to completion. I have more than 2 years of experience in ' +
-        'managing and leading teams across multiple sectors. I am eager to learn, I enjoy overcoming challenges, and I ' +
-        'have a genuine interest in Business Management and making organisations successful.',
-      edu: this.eduArray,
-      lang: this.langArray, empl: this.emplArray, skills: this.skillsArray, cources: 'Problem-solving and a drive to see things through to completion. ' +
-        'I have more than 2 years of experience in ' +
-        'managing and leading teams across multiple sectors. I am eager to learn, I enjoy overcoming challenges, and I '};
-    localStorage.removeItem('resume');
+    // const e: Educ = { start: new Date(), end: new Date(), degree: 'MS', school: 'Smolensk state university',
+    //                   decrpt: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy' +
+    //                     ' text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ' +
+    //                     'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,' +
+    //                     ' and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', city: 'Smolensl', level: ' ', years: ' '};
+    // this.eduArray.push(e);
+    // const empl: Empl = { start: new Date(), end: new Date(), job: 'Junior Full-Stack devdloper', empl: 'IT HUB',
+    //   decrpt: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard ' +
+    //     'dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ' +
+    //     ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop ' +
+    //     'publishing software like Aldus PageMaker including versions of Lorem Ipsum.', city: 'Smolensk'};
+    // this.emplArray.push(empl);
+    // const l1: Lang = {language: 'German', level: 'Beginner'};
+    // const l2: Lang = {language: 'English', level: 'Beginner'};
+    // this.langArray.push(l1);
+    // this.langArray.push(l2);
+    //
+    // const skill1: Skill = {skillValue: 8, skill: 'C++'};
+    // const skill2: Skill = {skillValue: 6, skill: 'Java'};
+    // const skill3: Skill = {skillValue: 3, skill: 'Python'};
+    // const skill4: Skill = {skillValue: 9, skill: 'HTML/CSS'};
+    // const skill5: Skill = {skillValue: 7, skill: 'Angular'};
+    // const skill6: Skill = {skillValue: 5, skill: 'Spring'};
+    // this.skillsArray.push(skill4);
+    // this.skillsArray.push(skill1);
+    // this.skillsArray.push(skill5);
+    // this.skillsArray.push(skill2);
+    // this.skillsArray.push(skill3);
+    //
+    // this.skillsArray.push(skill6);
+
+    // this.resume = {imagePreview: null, imagePath: this.res.imagePath,
+    //   firstName: 'Eva', secondName: 'Koroleva',
+    //   country: 'Europe', city: 'Sofia Mladost', citizenShip: 'World',
+    //   postCode: 'Street bl.53 2000', phone: '+123456789', email: 'myLovalyMail@mail.com', web: 'www.web',
+    //   skype: 'my_skype', gitHub: 'gitHub/account', profile: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' +
+    //     'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type ' +
+    //     'and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,' +
+    //     ' remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, ' +
+    //     'and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    //
+    //   edu: this.eduArray,
+    //   lang: this.langArray, empl: this.emplArray, skills: this.skillsArray, cources: 'Lorem Ipsum is simply dummy text of the printing and ' +
+    //     'typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley' +
+    //     ' of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, ' +
+    //     'remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently' +
+    //     ' with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'};
+   // localStorage.removeItem('resume');
   }
 
   public openPDF(): void {
@@ -72,42 +93,40 @@ export class PreviewComponent implements OnInit {
     // onePageCanvas.setAttribute('height', String(980));
     //
     const data = document.getElementById('htmlData');
-    html2canvas(data).then(canvas => {
-      const contentDataURL = canvas.toDataURL('image/png', 1);
-      const pdf = new jsPDF('p', 'mm', 'a4');
-      const width = pdf.internal.pageSize.getWidth();
-      const divHeight = this.htmlData.nativeElement.offsetHeight;
-      const divWidth = this.htmlData.nativeElement.offsetWidth;
-      const ratio = divHeight / divWidth;
-      const height = ratio * width;
-      pdf.addImage(contentDataURL, 'PNG', 10, 5, width - 20, height -5);
-      pdf.save('CV.pdf');
-    });
-
     // html2canvas(data).then(canvas => {
-    //   const doc = new jsPDF('p', 'px', 'a4');
     //   const contentDataURL = canvas.toDataURL('image/png', 1);
-    //   const pageHeight = doc.internal.pageSize.getHeight();
-    //   const pageWidth = doc.internal.pageSize.getWidth();
-    //
-    //   const imgheight = this.htmlData.nativeElement.offsetHeight;  // px to mm
-    //   const pagecount = Math.ceil(imgheight / pageHeight);
-    //   console.log(imgheight, pageHeight);
-    //   /* add initial page */
-    //   doc.addPage('p', 'px', 'a4');
-    //   doc.addImage(contentDataURL, 'PNG', 20, 10, pageWidth, 0);
-    //
-    //   /* add extra pages if the div size is larger than a a4 size */
-    //   if (pagecount > 0) {
-    //     let j = 1;
-    //     while (j !== pagecount) {
-    //       doc.addPage('p', 'mm', 'a4');
-    //       doc.addImage(contentDataURL, 'PNG', 20, 10, -(j * pageHeight), pageWidth - 4, 0);
-    //       j++;
-    //     }
-    //   }
-    //     doc.save('CV.pdf');
+    //   const pdf = new jsPDF('p', 'mm', 'a3');
+    //   const width = pdf.internal.pageSize.getWidth();
+    //   const divHeight = this.htmlData.nativeElement.offsetHeight;
+    //   const divWidth = this.htmlData.nativeElement.offsetWidth;
+    //   const ratio = divHeight / divWidth;
+    //   const height = ratio * width;
+    //   //pdf.addImage(contentDataURL, 'PNG', 0, 0, width, height);
+    //   pdf.addImage(contentDataURL, 'PNG', 5, 5, 200, 200);
+    //   pdf.save('CV.pdf');
     // });
-  }
+    html2canvas(data).then(canvas => {
+          let width = canvas.width;
+          let height = canvas.height;
+
+          const  imgData = canvas.toDataURL('image/jpeg');
+          const  doc = new jsPDF('p', 'mm', 'a4', true);
+          width = doc.internal.pageSize.width;
+          height = doc.internal.pageSize.height;
+          const img = new Image(),
+          ctx = canvas.getContext('2d');
+
+          const divHeight = this.htmlData.nativeElement.offsetHeight;
+          const divWidth = this.htmlData.nativeElement.offsetWidth;
+          const ratio = divHeight / divWidth;
+          // doc.addPage();
+           doc.addImage(imgData, 'JPEG', -1, -80, width, 370, undefined,'FAST');
+          //doc.addImage(imgData, 'JPEG', 2, 0, width-5, ratio * width, 'a', 'MEDIUM');
+            //doc.addImage(imgData, 'JPEG', 0, -50, width, height, undefined, 'FAST');
+          // //doc.addImage(imgData, 'JPEG', 0, 0);
+          doc.save('WebSiteScreen.pdf');
+
+  });
+ }
 
 }
